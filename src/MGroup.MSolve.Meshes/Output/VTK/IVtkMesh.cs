22324ed00fdf413fb3b2af3d90ab3d2e7397c4a1
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2024-02-28 00:22:29 +0200

    Imported MSolve.Meshes project

---
+++ src/MGroup.MSolve.Meshes/Output/VTK/IVtkMesh.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MGroup.MSolve.Meshes.Output.VTK
+{
+    public interface IVtkMesh
+    {
+        IReadOnlyList<VtkCell> VtkCells { get; } //TODO: Make this a dictionary
+
+        IReadOnlyList<VtkPoint> VtkPoints { get; } //TODO: Make this a dictionary
+    }
+}
