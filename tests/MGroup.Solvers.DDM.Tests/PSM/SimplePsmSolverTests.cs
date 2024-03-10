aae396e4e1058b3bee39ea2011521db378e8e353
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2024-03-10 16:15:54 +0200

    Imported PSM tests

---
+++ tests/MGroup.Solvers.DDM.Tests/PSM/SimplePsmSolverTests.cs
@@ -0,0 +1,186 @@
+using MGroup.Constitutive.Thermal;
+using MGroup.Environments;
+using MGroup.LinearAlgebra.Iterative;
+using MGroup.LinearAlgebra.Matrices;
+using MGroup.MSolve.Discretization.Entities;
+using MGroup.NumericalAnalyzers;
+using MGroup.Solvers.DDM.LinearSystem;
+using MGroup.Solvers.DDM.Psm;
+using MGroup.Solvers.DDM.PSM.InterfaceProblem;
+using MGroup.Solvers.DDM.PSM.StiffnessMatrices;
+using MGroup.Solvers.DDM.Tests.ExampleModels;
+using MGroup.Solvers.Results;
+
+using Xunit;
+
+namespace MGroup.Solvers.DDM.Tests.PSM
+{
+	public static class SimplePsmSolverTests
+	{
+		[Theory]
+		[InlineData(EnvironmentChoice.SequentialShared)]
+		[InlineData(EnvironmentChoice.TplShared)]
+		public static void TestForBrick3D(EnvironmentChoice environmentChoice)
+			=> TestForBrick3DInternal(environmentChoice.CreateEnvironment());
+
+		internal static void TestForBrick3DInternal(IComputeEnvironment environment)
+		{
+			// Environment
+			ComputeNodeTopology nodeTopology = Brick3DExample.CreateNodeTopology();
+			environment.Initialize(nodeTopology);
+
+			// Model
+			IModel model = Brick3DExample.CreateMultiSubdomainModel();
+			model.ConnectDataStructures(); //TODOMPI: this is also done in the analyzer
+
+			// Solver
+			var solverFactory = new PsmSolver<SymmetricCscMatrix>.Factory(
+				environment, new PsmSubdomainMatrixManagerSymmetricCSparse.Factory());
+			solverFactory.InterfaceProblemSolverFactory = new PsmInterfaceProblemSolverFactoryPcg()
+			{
+				MaxIterations = 200,
+				ResidualTolerance = 1E-10
+			};
+			DistributedAlgebraicModel<SymmetricCscMatrix> algebraicModel = solverFactory.BuildAlgebraicModel(model);
+			PsmSolver<SymmetricCscMatrix> solver = solverFactory.BuildSolver(model, algebraicModel);
+
+			// Linear static analysis
+			var problem = new ProblemThermal(model, algebraicModel);
+			var childAnalyzer = new LinearAnalyzer(algebraicModel, solver, problem);
+			var parentAnalyzer = new StaticAnalyzer(algebraicModel, problem, childAnalyzer);
+
+			// Run the analysis
+			parentAnalyzer.Initialize();
+			parentAnalyzer.Solve();
+
+			// Check results
+			NodalResults expectedResults = Brick3DExample.GetExpectedNodalValues(problem.ActiveDofs);
+			double tolerance = 1E-7;
+			environment.DoPerNode(subdomainID =>
+			{
+				NodalResults computedResults = algebraicModel.ExtractAllResults(subdomainID, solver.LinearSystem.Solution);
+				Assert.True(expectedResults.IsSuperSetOf(computedResults, tolerance, out string msg), msg);
+			});
+
+			//Debug.WriteLine($"Num PCG iterations = {solver.PcgStats.NumIterationsRequired}," +
+			//    $" final residual norm ratio = {solver.PcgStats.ResidualNormRatioEstimation}");
+
+			// Check convergence
+			int precision = 10;
+			int pcgIterationsExpected = 160;
+			double pcgResidualNormRatioExpected = 7.487370033127084E-11;
+			IterativeStatistics stats = solver.InterfaceProblemSolutionStats;
+			Assert.Equal(pcgIterationsExpected, stats.NumIterationsRequired);
+			Assert.Equal(pcgResidualNormRatioExpected, stats.ResidualNormRatioEstimation, precision);
+		}
+
+		[Theory]
+		[InlineData(EnvironmentChoice.SequentialShared)]
+		[InlineData(EnvironmentChoice.TplShared)]
+		public static void TestForLine1D(EnvironmentChoice environmentChoice)
+			=> TestForLine1DInternal(environmentChoice.CreateEnvironment());
+
+		internal static void TestForLine1DInternal(IComputeEnvironment environment)
+		{
+			// Environment
+			ComputeNodeTopology nodeTopology = Line1DExample.CreateNodeTopology();
+			environment.Initialize(nodeTopology);
+
+			// Model
+			IModel model = Line1DExample.CreateMultiSubdomainModel();
+			model.ConnectDataStructures(); //TODOMPI: this is also done in the analyzer
+
+			// Solver
+			var solverFactory = new PsmSolver<SymmetricCscMatrix>.Factory(
+				environment, new PsmSubdomainMatrixManagerSymmetricCSparse.Factory());
+			DistributedAlgebraicModel<SymmetricCscMatrix> algebraicModel = solverFactory.BuildAlgebraicModel(model);
+			PsmSolver<SymmetricCscMatrix> solver = solverFactory.BuildSolver(model, algebraicModel);
+
+			// Linear static analysis
+			var problem = new ProblemThermal(model, algebraicModel);
+			var childAnalyzer = new LinearAnalyzer(algebraicModel, solver, problem);
+			var parentAnalyzer = new StaticAnalyzer(algebraicModel, problem, childAnalyzer);
+
+			// Run the analysis
+			parentAnalyzer.Initialize();
+			parentAnalyzer.Solve();
+
+			// Check results
+			NodalResults expectedResults = Line1DExample.GetExpectedNodalValues(problem.ActiveDofs);
+			double tolerance = 1E-7;
+			environment.DoPerNode(subdomainID =>
+			{
+				NodalResults computedResults = algebraicModel.ExtractAllResults(subdomainID, solver.LinearSystem.Solution);
+				Assert.True(expectedResults.IsSuperSetOf(computedResults, tolerance, out string msg), msg);
+			});
+
+			//Debug.WriteLine($"Num PCG iterations = {solver.PcgStats.NumIterationsRequired}," +
+			//    $" final residual norm ratio = {solver.PcgStats.ResidualNormRatioEstimation}");
+
+			// Check convergence
+			int precision = 10;
+			int pcgIterationsExpected = 7;
+			double pcgResidualNormRatioExpected = 0;
+			IterativeStatistics stats = solver.InterfaceProblemSolutionStats;
+			Assert.Equal(pcgIterationsExpected, stats.NumIterationsRequired);
+			Assert.Equal(pcgResidualNormRatioExpected, stats.ResidualNormRatioEstimation, precision);
+		}
+
+		[Theory]
+		[InlineData(EnvironmentChoice.SequentialShared)]
+		[InlineData(EnvironmentChoice.TplShared)]
+		public static void TestForPlane2D(EnvironmentChoice environmentChoice)
+			=> TestForPlane2DInternal(environmentChoice.CreateEnvironment());
+
+		internal static void TestForPlane2DInternal(IComputeEnvironment environment)
+		{
+			// Environment
+			ComputeNodeTopology nodeTopology = Plane2DExample.CreateNodeTopology();
+			environment.Initialize(nodeTopology);
+
+			// Model
+			IModel model = Plane2DExample.CreateMultiSubdomainModel();
+			model.ConnectDataStructures(); //TODOMPI: this is also done in the analyzer
+
+			// Solver
+			var solverFactory = new PsmSolver<SymmetricCscMatrix>.Factory(
+				environment, new PsmSubdomainMatrixManagerSymmetricCSparse.Factory());
+			solverFactory.InterfaceProblemSolverFactory = new PsmInterfaceProblemSolverFactoryPcg()
+			{
+				MaxIterations = 200,
+				ResidualTolerance = 1E-10
+			};
+			DistributedAlgebraicModel<SymmetricCscMatrix> algebraicModel = solverFactory.BuildAlgebraicModel(model);
+			PsmSolver<SymmetricCscMatrix> solver = solverFactory.BuildSolver(model, algebraicModel);
+
+			// Linear static analysis
+			var problem = new ProblemThermal(model, algebraicModel);
+			var childAnalyzer = new LinearAnalyzer(algebraicModel, solver, problem);
+			var parentAnalyzer = new StaticAnalyzer(algebraicModel, problem, childAnalyzer);
+
+			// Run the analysis
+			parentAnalyzer.Initialize();
+			parentAnalyzer.Solve();
+
+			// Check results
+			NodalResults expectedResults = Plane2DExample.GetExpectedNodalValues(problem.ActiveDofs);
+			double tolerance = 1E-7;
+			environment.DoPerNode(subdomainID =>
+			{
+				NodalResults computedResults = algebraicModel.ExtractAllResults(subdomainID, solver.LinearSystem.Solution);
+				Assert.True(expectedResults.IsSuperSetOf(computedResults, tolerance, out string msg), msg);
+			});
+
+			//Debug.WriteLine($"Num PCG iterations = {solver.PcgStats.NumIterationsRequired}," +
+			//    $" final residual norm ratio = {solver.PcgStats.ResidualNormRatioEstimation}");
+
+			// Check convergence
+			int precision = 10;
+			int pcgIterationsExpected = 63;
+			double pcgResidualNormRatioExpected = 4.859075883397028E-11;
+			IterativeStatistics stats = solver.InterfaceProblemSolutionStats;
+			Assert.Equal(pcgIterationsExpected, stats.NumIterationsRequired);
+			Assert.Equal(pcgResidualNormRatioExpected, stats.ResidualNormRatioEstimation, precision);
+		}
+	}
+}
