ff90900008281fd9d42fa63ea214a004b3a0edf0
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2024-05-02 18:34:53 +0300

    Update to latest LinearAlgebra version

--- src/MGroup.Solvers/AlgebraicModel/GlobalAlgebraicModel.cs
+++ src/MGroup.Solvers/AlgebraicModel/GlobalAlgebraicModel.cs
@@ -16,7 +16,7 @@ using MGroup.Solvers.Assemblers;
 using MGroup.Solvers.DofOrdering;
 using MGroup.Solvers.LinearSystem;
 using MGroup.Solvers.Results;
-using MGroup.LinearAlgebra.LinearAlgebraExtensions;
+using MGroup.LinearAlgebra.Exceptions;
 
 namespace MGroup.Solvers.AlgebraicModel
 {
